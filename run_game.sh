084f9a7f3701cc9f8c3d951918902f67a9ed2679
andreljungsvik321 <andlar0408@skola.goteborg.se>
2024-10-24 16:20:20 +0200

    Added run script for the game

---
+++ run_game.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+NAME=$USER
+DIRECTORY="${NAME}_labb"
+
+echo $NAME"s program"
+mkdir "$DIRECTORY"
+cp GuessingGame.java Guesser.java "$DIRECTORY/"
+cd "$DIRECTORY"
+echo "Running game from $(pwd)"
+echo "compiling..."
+javac GuessingGame.java
+echo "running..."
+java GuessingGame
+echo "Done!"
+echo "Removing class files..."
+rm *.class
+ls
